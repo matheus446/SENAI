c09545602d9fb1e76fc3bf93dc7ff41dc20cc01c
matheus446 <matheus.f.andrade8@aluno.senai.br>
2026-03-05 12:13:47 -0300

    back

--- Backend/APIs/api_cinema/src/config/database.js
+++ Backend/APIs/api_cinema/src/config/database.js
@@ -4,7 +4,7 @@ require('dotenv').config()
 
 const pool = mysql.createPool({
     host: process.env.DB_HOST,
-    qser: process.env.DB_USER,
+    user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     port: process.env.DB_PORT,
--- Backend/APIs/api_cinema/src/server.js
+++ Backend/APIs/api_cinema/src/server.js
@@ -1,17 +1,38 @@
-const pool = require('./config/database')
-const app = require('./app')
+// const pool = require('./config/database')
+// const app = require('./app')
 
-const PORT = 3000
+// const PORT = 3000
+
+// pool.getConnection((err, connection) => {
+//     if(err){
+//         console.error('erro ao conectar ao banco:', err)
+//         process.exit(1)
+//     }
+//     console.log('conectado ao MYSQL')
+//     connection.release()
+// })
+
+// app.listen(PORT, ()=>{
+//     console.log('servidor rodando!!!')
+// })
+
+
+
+const app = require('./app');
+const pool = require('./config/database');
+
+const PORT = 3000;
 
 pool.getConnection((err, connection) => {
-    if(err){
-        console.error('erro ao conectar ao banco:', err)
-        process.exit(1)
+    if (err) {
+        console.error('Erro ao conectar no banco:', err);
+        process.exit(1);
     }
-    console.log('conectado ao MYSQL')
-    connection.release()
-})
 
-app.listen(PORT, ()=>{
-    console.log('servidor rodando!!!')
-})
+    console.log('Conectado ao MySQL com sucesso! 🎉');
+    connection.release();
+});
+
+app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`);
+});
